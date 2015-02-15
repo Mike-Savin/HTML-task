d9de65587f29d17740d4a65ea8ae8e6981c04f68
Michael Savin <my.savin@yandex.com>
2015-02-15 17:57:20 +0200

    Finish top navigation

--- js/home.js
+++ js/home.js
@@ -1,3 +1,9 @@
 $('#header_search_icon').click(function(){
 	$('#header_input').focus();
 });
+
+$('#top_nav_ul').delegate('li', 'click', function(){
+	$('.green-line').css('left', 13 + 190 * $(this).attr('data-number'));
+	$('#top_nav_ul li').removeClass('active');
+	$(this).addClass('active');
+});
